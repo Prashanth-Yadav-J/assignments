99a4e31f64cb47f29ec473983a93485780d5e8ac
Prashanth-Yadav-J <prashanthyadav.j@gmail.com>
2025-11-03 14:35:08 +0530

    Assignment01 Datatypes

---
+++ src/assignments/Assignment01.java
@@ -0,0 +1,117 @@
+package assignments;
+
+public class Assignment01 {
+	
+	/**Following is Assignment and here following data types are used 
+	1)Primitive Data Types - int,short,float,double,boolean,char
+    2)Non Primitive Data Types - String*/
+
+	public static void main(String[] args) {
+		
+		//1.Temperature of a city in degrees Celsius: 25.5
+		Float temperaturecelsius = 25.2f;
+		System.out.println("1.Temperature of a city in degrees Celsius:"+temperaturecelsius);
+		
+		//2.Whether a customer has placed an order: true or false
+		
+		/*Executing 2nd one multiple times
+		 1.True and 2.False*/
+		Boolean orderpass=true;
+		Boolean orderfail=false;
+		System.out.println("2.Whether a customer has placed an order:"+orderpass);
+		System.out.println("2.Whether a customer has placed an order:"+orderfail);
+		
+		//3.Person's phone number: "123-456-7890"
+		String phonenumber="123-456-7890";
+		System.out.println("3.Person's phone number:"+phonenumber);
+		
+		//4.Amount of money in a customer's bank account: 1000.50
+		
+		/*Executing 4th one multiple times using datatypes
+		 1.Float and 2.Double*/
+		
+		Float amount=1000.50f;
+		System.out.println("4.Amount of money in a customer's bank account:"+amount);
+		
+		Double amount1=1000.50;
+		System.out.println("4.Amount of money in a customer's bank account:"+amount1);
+		
+		//5.Person's email address: "john.doe@example.com"
+		String emailid="john.doe@example.com";
+		System.out.println("5.Person's email address:"+emailid);
+		
+		//6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		Float latitude=37.7749f;
+		Float longitude=-122.4194f;
+		System.out.println("6.Coordinates of a location (latitude, longitude):"+latitude +longitude);
+		
+		//7.Person's marital status: true or false
+		
+		/*Executing 7th one multiple times
+		 1.True and 2.False*/
+		
+		Boolean married=true;
+		Boolean unmarried=false;
+		System.out.println("7.Person's marital status:"+married);
+		System.out.println("7.Person's marital status:"+unmarried);
+		
+		//8.Person's occupation: "Software Engineer"
+		String occupation="Software Engineer";
+		System.out.println("8.Person's occupation:"+occupation);
+		
+		//9.Person's favourite colour: "Blue"
+		String colourblue="Blue";
+		System.out.println("9.Person's favourite colour:"+colourblue);
+		
+		//10.Current year: 2023
+		Short year=2023;
+		Short currentyear=2025;
+		System.out.println("10.Current year:"+year);
+		System.out.println("10.Current year:"+currentyear);
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		int followerscount=1000000;
+		System.out.println("11.Number of followers on a social media platform:"+followerscount);
+		
+		//12.Rating of a movie: 7.5
+		Float rating=7.5f;
+		System.out.println("12.Rating of a movie:"+rating);
+		
+		//13.Person's blood type: 'A'
+		char bloodgroup='A';
+		System.out.println("13.Person's blood type:"+bloodgroup);
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String bookname="To Kill a Mockingbird";
+		System.out.println("14.Title of a book:"+bookname);
+		
+		//15.Number of employees in a company: 500
+		short employeescount=500;
+		System.out.println("15.Number of employees in a company:"+employeescount);
+		
+		//16.Time of an event: 2:30 PM
+		String time="2:30";
+		String meridianpm="PM";
+		String meridianam="AM";
+		System.out.println("16.Time of an event:"+time +meridianpm);
+		
+		//17.Name of a country: "United States"
+		String countryname="United States";
+		System.out.println("17.Name of a country:"+countryname);
+		
+		//18.Person's eye color: "Brown"
+		String colourbrown="Brown";
+		System.out.println("18.Person's eye color:"+colourbrown);
+		
+		//19.Person's birthplace: "New York City"
+		String americancity="New York City";
+		System.out.println("19.Person's birthplace:"+americancity);
+		
+		//20.Distance between two cities: 200.5
+		Float distance=200.5f;
+		String distancemeasurement="KMS";
+		System.out.println("20.Distance between two cities:"+distance +distancemeasurement);
+
+	}
+
+}
